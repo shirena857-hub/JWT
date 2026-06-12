483d6fb5007552faceb978b1601aab50dedd30a6
shirena857-hub <shirena857@gmail.com>
2026-06-12 06:19:44 +0600

    Delete GetLongBio_Vio_Api.py

--- GetLongBio_Vio_Api.py
+++
@@ -1,6 +0,0 @@
-import requests
-bio = input("Enter Bio: ")
-token = input("Enter Token: ")
-url = f"https://foxlongbio.vercel.app/encrypt?bio={bio}&token={token}"
-res = requests.get(url)
-print(res.json())
